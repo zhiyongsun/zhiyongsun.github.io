97283831816fb01a88863ce2581619254abbebeb
sunzhiyong7 <sunzhiyong@jd.com>
2018-06-20 20:14:01 +0800

    initial

--- app.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by zhiyong on 15/08/2017.
- */
-var exec = require('child_process').exec;
-exec("hexo server",function(){})
--- blog.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by zhiyong on 27/08/2017.
- */
-require(‘hexo’).init({command: ‘server’});
