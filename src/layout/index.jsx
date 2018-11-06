cf53aacaeea1a5f411009e9cde858bcbbbae0d99
sunzhiyong036 <785037725@qq.com>
2018-11-06 17:41:50 +0800

    container 样式调整

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -7,7 +7,7 @@ export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div>
+      <div className="container">
         <Helmet>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
