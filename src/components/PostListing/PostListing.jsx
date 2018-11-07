c9d10e93015cdc63791d7b45f13369699a5942ba
sunzhiyong7 <785037725@qq.com>
2018-11-07 23:23:39 +0800

    增加时间

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -1,37 +1,42 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react'
+import { Link } from 'gatsby'
+const getDate = require('../../../common/utils/date').getDate;
 
 class PostListing extends React.Component {
   getPostList() {
-    const postList = [];
+    const postList = []
     this.props.postEdges.forEach(postEdge => {
       postList.push({
         path: postEdge.node.fields.slug,
         tags: postEdge.node.frontmatter.tags,
         cover: postEdge.node.frontmatter.cover,
         title: postEdge.node.frontmatter.title,
-        date: postEdge.node.fields.date,
+        date: getDate(postEdge.node.fields.date),
         excerpt: postEdge.node.excerpt,
         timeToRead: postEdge.node.timeToRead
-      });
-    });
-    return postList;
+      })
+    })
+    return postList
   }
+
   render() {
-    const postList = this.getPostList();
+    const postList = this.getPostList()
     return (
       <div>
         {/* Your post list here. */
-        postList.map(post => (
-          <Link to={post.path} key={post.title}>
-            <h1>{post.title}</h1>
-            {/*<span>{post.date}</span>*/}
-            {/*<p>{post.excerpt}</p>*/}
-          </Link>
-        ))}
+          postList.map(post => (
+            <div>
+              {post.date}：
+              <Link to={post.path} key={post.title}>
+                {post.title}
+                {/*<span>{post.date}</span>*/}
+                {/*<p>{post.excerpt}</p>*/}
+              </Link>
+            </div>
+          ))}
       </div>
-    );
+    )
   }
 }
 
-export default PostListing;
+export default PostListing
