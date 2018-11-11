4b49e1ebbf8b6d1282188e05230e54d049313c2b
sunzhiyong7 <785037725@qq.com>
2018-11-11 17:56:24 +0800

    why ??

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,12 +4,14 @@ module.exports = {
   siteTitleAlt: "孙志勇的个人网站", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.jpg", // Logo used for SEO and manifest.
   siteUrl: "https://www.sunzhiyong.cn", // Domain of your website without pathPrefix.
+  // siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
   pathPrefix: "/gatsby-advanced-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "孙志勇的个人网站", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
   disqusShortname: "sunzhiyong", // Disqus shortname.
+  // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
