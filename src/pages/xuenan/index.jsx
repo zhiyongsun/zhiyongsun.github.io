3545c2b10015cb57f4f73bae9b032642c03ee1d1
sunzhiyong7 <785037725@qq.com>
2018-11-26 22:04:33 +0800

    add xuenan file

---
+++ src/pages/xuenan/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../../layout'
+import PostListing from '../../components/PostListing/PostListing'
+import SEO from '../../components/SEO/SEO'
+import config from '../../../data/SiteConfig'
+
+class Index extends React.Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Layout>
+      <div>oweiofe</div>
+      </Layout>
+    )
+  }
+}
+
+export default Index
