f0b97fcf65817bc9f7a583bc3e81134b13785f95
sunzhiyong7 <sunzhiyong@jd.com>
2018-12-12 15:42:09 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2b18a8dfac345bbe170d.js"
+    "url": "webpack-runtime-bb7740951eb0d6832cf0.js"
   },
   {
     "url": "app-f2934ba2bf5997c615e4.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "71d74b8f9a5980a6bed86da54a9c5767"
+    "revision": "a7eac5c4538cf8b1e181257e00396072"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6d2303f3dbb5cd06021994a022fa1131"
+    "revision": "281a34ee5266f0596a02a8a7d2dedc1e"
   },
   {
     "url": "component---src-pages-index-jsx.e0bea7d8c28b8cfbdb63.css"
@@ -52,8 +52,8 @@ self.__precacheManifest = [
     "url": "0-8b278b84e526329ac2a6.js"
   },
   {
-    "url": "static/d/357/path---index-6a9-3bgjZGEDb9NFlsO0cIomZ23M764.json",
-    "revision": "0a99f34d03152f1520404f61adb91679"
+    "url": "static/d/995/path---index-6a9-8XFa4viHWFmiC08d8WGzPbb57zY.json",
+    "revision": "e0554bec0546f7aeeaf04f2941afb857"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
